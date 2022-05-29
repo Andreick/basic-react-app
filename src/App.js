39eb14227e40ff9de1b2e2b5850c71725776a824
Andreick <andreick.gomes.ismart@gmail.com>
2022-05-29 01:21:35 -0300

    add input forms

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import './App.css';
-import NumberList from './NumberList';
+import UserForm from './UserForm';
 
 function App() {
-  const numbers = [23, 45, 64, 78, 'test'];
-  const numbers2 = [21, 45, 56, 78, 'test'];
-  const todos = [
-    { id: '1', name: 'Coding', isDone: false },
-    { id: '2', name: 'Walking', isDone: true },
-  ];
   return (
     <div>
-      <NumberList numbers={numbers} />
-      <NumberList numbers={numbers2} />
-      <ul>
-        {todos.map((todo) => {
-          return (
-            <li key={todo.id}>
-              {todo.name}
-              {todo.isDone && ' - Done'}
-            </li>
-          );
-        })}
-      </ul>
+      <UserForm />
     </div>
   );
 }
