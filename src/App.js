74c797a8352f9f21e8d64203cff4dc0db72f3246
Andreick <andreick.gomes.ismart@gmail.com>
2022-05-29 00:17:38 -0300

    Lists and keys

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
-import LoginControl from './LoginControl';
+import NumberList from './NumberList';
 
 function App() {
+  const numbers = [23, 45, 64, 78, 'test'];
+  const numbers2 = [21, 45, 56, 78, 'test'];
+  const todos = [
+    { id: '1', name: 'Coding', isDone: false },
+    { id: '2', name: 'Walking', isDone: true },
+  ];
   return (
     <div>
-      <LoginControl hasNewMessage credit={100} hasWarning />
+      <NumberList numbers={numbers} />
+      <NumberList numbers={numbers2} />
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              {todo.name}
+              {todo.isDone && ' - Done'}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
